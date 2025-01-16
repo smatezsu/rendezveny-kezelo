59a3aaa0d8885f3901b426fc40fe1ccfe9a67a1e
smatezsu <schveighardt.mate@gmail.com>
2025-01-16 19:48:33 +0100

    toString metódus

--- rendezveny-kezelo/src/models/participant.ts
+++ rendezveny-kezelo/src/models/participant.ts
@@ -2,4 +2,9 @@
 
 export class Participant {
     constructor(public name: string, public email: string) {}
-  }  
+  
+    toString(): string {
+      return `${this.name} (${this.email})`;
+    }
+  }
+  
