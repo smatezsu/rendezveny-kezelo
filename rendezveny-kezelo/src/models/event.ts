fc0201e986ddb3d44e236eaa374ea7f471ae0959
smatezsu <schveighardt.mate@gmail.com>
2025-01-16 19:39:45 +0100

    résztvevők törlése

--- rendezveny-kezelo/src/models/event.ts
+++ rendezveny-kezelo/src/models/event.ts
@@ -1,28 +1,12 @@
 // ESEMÉNY ADATOK //
 
-import { Participant } from "./participant";
 import { EventType } from "./eventType";
 
 export class Event {
-  public participants: Participant[] = [];
-
   constructor(
     public name: string,
     public location: string,
     public date: Date,
     public type: EventType
   ) {}
-
-  addParticipant(participant: Participant): void {
-    this.participants.push(participant);
-  }
-
-  removeParticipant(email: string): boolean {
-    const index = this.participants.findIndex(p => p.email === email);
-    if (index !== -1) {
-      this.participants.splice(index, 1);
-      return true;
-    }
-    return false;
-  }
 }
